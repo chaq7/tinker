7f7468575ded79c53b8fffefbd6e9475a652e6f1
kazuma <dru-m-usic.dec7@docomo.ne.jp>
2022-03-05 09:10:10 +0000

    リファクタリング

--- app/controllers/weather_forecast_controller.rb
+++
@@ -1,4 +0,0 @@
-class WeatherForecastController < ApplicationController
-  def index
-  end
-end
--- app/helpers/weather_forecast_helper.rb
+++
@@ -1,7 +0,0 @@
-module WeatherForecastHelper
-
-    def specify_city_weather_forecasts(city_id)
-      WeatherForecast.specify_city(city_id)
-    end
-
-end
