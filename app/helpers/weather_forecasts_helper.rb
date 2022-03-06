6b7a005a230b80eda68703790ef124cf582e17d1
kazuma <dru-m-usic.dec7@docomo.ne.jp>
2022-03-06 12:07:00 +0000

    メソッド名を変更

--- app/helpers/weather_forecasts_helper.rb
+++ app/helpers/weather_forecasts_helper.rb
@@ -2,7 +2,7 @@ module WeatherForecastsHelper
 
   def my_city_weather_forecasts(user_id)
     city_ids = CityUser.my_city_ids(user_id)
-    WeatherForecast.my_weather_forecasts(city_ids)
+    WeatherForecast.target_cities(city_ids)
   end
 
 end
